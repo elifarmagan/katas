cec9ec4f2623bdd92ada50d334b52724069857c6
Elif Armagan <esarmagan@gtu.edu.tr>
2019-06-04 12:58:40 +0300

    added factorial solution

---
+++ src/Factorial.java
@@ -0,0 +1,20 @@
+public class Factorial {
+
+    public static void main(String[] args) {
+        int res = factorial(2);
+        System.out.println(res);
+
+    }
+    public static int factorial(int n) {
+        if(n < 0 || n > 12)
+            throw new IllegalArgumentException();
+
+        int result = 1;
+        while (n > 0) {
+            result *= n;
+            n--;
+        }
+
+        return result ;
+    }
+}
